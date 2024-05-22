13b38fdc27b336f05c725ed75c5e3bbd8765719e
kerfun <kevin@hofa.com>
2024-05-22 10:27:52 -0600

    changed setup

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
 import setuptools
-
+import os
 from _version import __version__ as version
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("Boto4GS/requirements.txt", "r", encoding="utf-8") as fh:
-    install_requires = fh.read().splitlines()
+requirements = ""
+with open("PyIssues/requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+requirements = requirements.split("\n")
+
+def listFolders(directory):
+    folders = []
+    for item in os.listdir(directory):
+        itemPath = os.path.join(directory, item)
+        if os.path.isdir(itemPath) and item != "__pycache__":
+            folders.append(itemPath)
+    otherFolders = [listFolders(itemPath) for itemPath in folders]
+    for folder in otherFolders:
+        folders.extend(folder)
+    return folders
+
+folderPath = "PyIssues"
+folders = listFolders(folderPath)
+folders.append("PyIssues")
+print(folders)
 
 setuptools.setup(
-    name="Boto4GS",
+    name='PyIssues',
     version=version,
-    author="Record Linking Lab",
-    author_email="recordlinkinglab@gmail.com",
-    description="A package to interact with the RLL AWS account in a set way",
+    author='Record Linking Lab',
+    author_email='recordlinkinglab@gmail.com',
+    description='A python library for catching thrown exceptions and automatically creating issues on a GitHub repo.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/byuawsfhtl/Boto4GS.git",
-    project_urls={
-        "Bug Tracker": "https://github.com/byuawsfhtl/Boto4GS/issues",
+    url='https://github.com/byuawsfhtl/PyIssues.git',
+    project_urls = {
+        "Bug Tracker": "https://github.com/byuawsfhtl/PyIssues/issues"
     },
-    packages=["Boto4GS", "Boto4GS/src", "Boto4GS/src/models", "Boto4GS/src/models/services", "Boto4GS/src/presenters", "Boto4GS/src/views"],
-    install_requires=install_requires,
+    packages=folders,
+    install_requires=requirements,
 )
